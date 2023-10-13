614f61198f056578857f44744a22c5aa2fc8dfd6
Sachin <sachinasodekar@gail.com>
2023-10-13 17:42:26 +0530

    Created Project Added Completed Sidebar Componenent

---
+++ src/utils/reducer.js
@@ -0,0 +1,31 @@
+import { reducerCases } from "./Constants";
+
+
+export const initialState = {
+    token: null,
+    userInfo: null,
+    playlists: [],
+    currentPlaying: null,
+    playerState: false,
+    selectedPlaylist: null,
+    selectedPlaylistId: "37i9dQZF1E37jO8SiMT0yN",
+  };
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case reducerCases.SET_TOKEN:
+            return {
+                ...state,
+                token: action.token,
+            };
+        case reducerCases.SET_PLAYLISTS:
+            return{
+                ...state,
+                playlists: action.playlists,
+            };
+        default:
+            return state;
+    }
+}
+
+export default reducer;
